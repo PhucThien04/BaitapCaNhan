aef36c8aea21cd261c35a0634e9305338be8924b
PhucThien04 <phucthiensh@gmail.com>
2024-10-08 20:03:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baitapcanhan/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.baitapcanhan;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    ViewPagerAdapter viewPagerAdapter;
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPaper);
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(viewPagerAdapter);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        frameLayout = findViewById(R.id.frameLayout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setVisibility(View.VISIBLE);
+                frameLayout.setVisibility(View.GONE);
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager2.setVisibility(View.VISIBLE);
+                frameLayout.setVisibility(View.GONE);
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                    case 1:
+                    case 2:
+                        tabLayout.getTabAt(position).select();
+                }
+                super.onPageSelected(position);
+            }
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                frameLayout.setVisibility(View.VISIBLE);
+                viewPager2.setVisibility(View.GONE);
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.bottom_home){
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frameLayout, new FragmentHome()).commit();
+                    return true;
+                }else if (itemId == R.id.bottom_profile){
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frameLayout, new FragmentProfile()).commit(); // Chú ý thay đổi FragmentSearch
+                    return true;
+                }else if (itemId == R.id.bottom_support){
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frameLayout, new FragmentSupport()).commit(); // Chú ý thay đổi FragmentProfile
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
